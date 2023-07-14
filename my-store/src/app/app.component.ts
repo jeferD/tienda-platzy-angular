cc28a9de017f6a139d72ef59da7982f0759a276a
Jeferson Delgado <92939552+jeferD@users.noreply.github.com>
2023-07-14 18:01:09 -0500

    estado

--- my-store/src/app/app.component.ts
+++ my-store/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'] // css del componente, se llama igual que el html menos el .ts
 })
 export class AppComponent {
   name = 'Jefer';
@@ -13,6 +13,27 @@ export class AppComponent {
   persona = {
     name:'Fabian',
     age: 24
+  }
+
+  toggleButton(){
+    this.btnDisabled = !this.btnDisabled;
+  }
+
+  increaseAge(){
+    this.persona.age +=1;
+  }
+
+  onScroll(event: Event){
+    const element = event.target as HTMLElement;
+    console.log('element: ', element.scrollTop);
+      
+  }
+
+  changeName(event: Event){
+    const element = event.target as HTMLInputElement;
+    console.log('element: ', element.value);
+    this.persona.name = element.value;
+
   }
 }
 
